5bf1afa17fd7b02f249203e466dc082fcfc211b9
Samiran Kawtikwar <samkawtikwar@gmail.com>
2024-09-28 11:03:02 -0500

    Refactor worker_info struct and memory allocation, change init_branching

--- branch.cuh
+++ branch.cuh
@@ -10,15 +10,14 @@
 #include "LAP/Hung_Tlap.cuh"
 
 __global__ void initial_branching(queue_callee(memory_queue, tickets, head, tail), uint memory_queue_size,
-                                  uint *addresses_space, node_info *node_space,
-                                  const problem_info *pinfo,
+                                  node_info *node_space, const problem_info *pinfo,
                                   queue_callee(request_queue, tickets, head, tail), uint request_queue_size,
-                                  queue_info *queue_space, work_info *work_space, BHEAP<node> bheap,
-                                  bool *hold_status,
-                                  const cost_type UB)
+                                  queue_info *queue_space, worker_info *work_space, BHEAP<node> bheap,
+                                  bool *hold_status, const cost_type UB)
 {
   const uint bId = blockIdx.x, psize = pinfo->psize;
-  uint *my_addresses = &addresses_space[bId * psize];
+  uint *my_addresses = work_space[bId].address_space;
+  worker_info *my_space = &work_space[bId];
   if (bId > 0)
   {
     __shared__ uint nchild, child_index;
@@ -28,36 +27,74 @@ __global__ void initial_branching(queue_callee(memory_queue, tickets, head, tail
       child_index = 0;
     }
     __syncthreads();
-    // get nchildren
-    for (uint i = threadIdx.x; i < psize; i += blockDim.x)
+    // get nchildren and their bounds
+    for (uint child_id = 0; child_id < psize; child_id++)
     {
-      cost_type LB = pinfo->costs[i * psize];
-      if (LB < UB)
+      if (threadIdx.x == 0)
       {
-        atomicAdd(&nchild, 1);
+        my_space->fixed_assignments[psize * child_id + child_id] = 1;
+        my_space->level[child_id] = 1;
+        my_space->LB[child_id] = (float)pinfo->costs[child_id * psize];
+      }
+      __syncthreads();
+      if (threadIdx.x == 0)
+      {
+        if (my_space->LB[child_id] < UB)
+        {
+          my_space->feasible[child_id] = true;
+          atomicAdd(&nchild, 1);
+        }
+        else
+          my_space->feasible[child_id] = false;
       }
     }
     __syncthreads();
+    if (threadIdx.x == 0)
+    {
+      printf("Block %u has %u feasible children\n", bId, nchild);
+    }
+    __syncthreads();
+    // Get nchild addresses
     get_memory(queue_caller(memory_queue, tickets, head, tail), memory_queue_size, nchild,
                my_addresses);
+
+    // construct a for sending to queue
     node *a = work_space[bId].nodes;
-
-    for (uint i = threadIdx.x; i < psize; i += blockDim.x)
+    __syncthreads();
+    for (uint child_id = threadIdx.x; child_id < psize; child_id += blockDim.x)
     {
-      uint my_index = atomicAdd(&child_index, 1);
-      node_info *b = &node_space[my_addresses[my_index]];
-      b->fixed_assignments[i] = 1;            // fix row i to column lvl + 1.
-      b->LB = (float)pinfo->costs[i * psize]; // Does not perform subgradient here just naive LB
-      b->level = 1;
-      a[my_index].value = b;
-      a[my_index].key = (float)pinfo->costs[i * psize];
+      if (my_space->feasible[child_id])
+      {
+        uint my_index = atomicAdd(&child_index, 1);
+        node_info *b = &node_space[my_addresses[my_index]];
 
+        // copy fixed assignments
+        for (uint j = 0; j < psize; j++)
+          b->fixed_assignments[j] = my_space->fixed_assignments[psize * child_id + j]; // fix row i to column lvl + 1.
+
+        b->LB = my_space->LB[child_id];
+        b->level = my_space->level[child_id];
+        a[my_index].value = b;
+        a[my_index].key = my_space->LB[child_id];
+      }
       // printf("Key: %f\n", a[i].key);
     }
+
     __syncthreads();
     send_requests(BATCH_PUSH, nchild, a,
                   queue_caller(request_queue, tickets, head, tail),
                   request_queue_size, queue_space);
+    __syncthreads();
+
+    // reset my_space
+    for (uint i = threadIdx.x; i < psize; i += blockDim.x)
+    {
+      my_space->LB[i] = 0;
+      my_space->feasible[i] = false;
+      my_space->level[i] = 0;
+      for (uint j = 0; j < psize; j++)
+        my_space->fixed_assignments[psize * i + j] = 0;
+    }
   }
   else
   {
@@ -73,10 +110,9 @@ __global__ void initial_branching(queue_callee(memory_queue, tickets, head, tail
 // Add launch bounds
 __launch_bounds__(n_threads, 2048 / n_threads)
     __global__ void branch_n_bound(queue_callee(memory_queue, tickets, head, tail), uint memory_queue_size,
-                                   uint *addresses_space, node_info *node_space, subgrad_space *subgrad_space,
-                                   const problem_info *pinfo,
+                                   node_info *node_space, subgrad_space *subgrad_space, const problem_info *pinfo,
                                    queue_callee(request_queue, tickets, head, tail), uint request_queue_size,
-                                   queue_info *queue_space, work_info *work_space, BHEAP<node> bheap,
+                                   queue_info *queue_space, worker_info *work_space, BHEAP<node> bheap,
                                    bool *hold_status,
                                    const cost_type global_UB,
                                    bnb_stats *stats)
@@ -95,7 +131,7 @@ __launch_bounds__(n_threads, 2048 / n_threads)
 
     if (threadIdx.x == 0)
     {
-      my_addresses = &addresses_space[blockIdx.x * psize];
+      my_addresses = work_space[blockIdx.x].address_space;
       // Needed for feasibility check
       col_fa = &subgrad_space->col_fixed_assignments[blockIdx.x * psize];
       lap_costs = &subgrad_space->lap_costs[blockIdx.x * psize * psize]; // subgradient always works with floats
